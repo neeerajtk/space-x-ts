626429e555ccc81f26511ab1eb1c0809783f7dd9
neeerajtk <neerajtk560@gmail.com>
2020-10-30 15:49:01 +0530

    basic cleanup

--- space/src/App.tsx
+++ space/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
-import Header from './components/Header';
+import DemoHeader from './components/DemoHeader';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <DemoHeader/>
     </div>
   );
 }

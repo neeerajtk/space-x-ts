3c8a5619c76a159887a441264f773d8fb932110a
neeerajtk <neerajtk560@gmail.com>
2020-10-30 23:56:56 +0530

    card added

--- space/src/App.tsx
+++ space/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import DemoHeader from './components/DemoHeader';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <DemoHeader/>
+      <Card/>
     </div>
   );
 }

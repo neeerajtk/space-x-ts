a549ea12a984b9874553142eeeac77ca8b17f6a5
neeerajtk <neerajtk560@gmail.com>
2020-10-31 00:08:02 +0530

    card err

--- space/src/App.tsx
+++ space/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import DemoHeader from './components/DemoHeader';
 import Card from './components/Card';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <DemoHeader/>
+      <Header/>
       <Card/>
     </div>
   );
--- space/src/components/Header.tsx
+++ space/src/components/Header.tsx
@@ -1,38 +1,37 @@
-// import ReactDOM from 'react-dom';
-// // import '@elastic/eui/dist/eui_theme_dark.css';
-// import '@elastic/eui/dist/eui_theme_light.css'
-// import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+// import '@elastic/eui/dist/eui_theme_dark.css';
+import '@elastic/eui/dist/eui_theme_light.css'
+import React, { useState } from 'react';
 
-// import {
-//   EuiHeader,
-//   EuiHeaderLogo,
-//   EuiSwitch,
-//   EuiSpacer,
-// } from '@elastic/eui';
+import {
+  EuiHeader,
+  EuiHeaderLogo,
+  EuiSwitch,
+  EuiSpacer,
+} from '@elastic/eui';
 
-// const Header = () => {
-//   const [position, setPosition] = useState('static');
+const Header = () => {
+  const [position, setPosition] = useState('static');
 
-//   const sections = [
-//     {
-//       items: [<EuiHeaderLogo>Elastic</EuiHeaderLogo>],
-//       borders: 'right',
-//     },
-//   ];
+  const sections = [
+    {
+      items: [<EuiHeaderLogo>Elastic</EuiHeaderLogo>],
+      borders: 'right',
+    },
+  ];
 
-//   return (
-//     <>
-//       <EuiSwitch
-//         label={'Make header fixed position'}
-//         checked={position === 'fixed'}
-//         onChange={(e) => setPosition(e.target.checked ? 'fixed' : 'static')}
-//       />
-//       <EuiSpacer />
-//       <EuiHeader position={position} sections={sections} />
-//     </>
-//   );
-// };
+  return (
+    <>
+      <EuiSwitch
+        label={'Make header fixed position'}
+        checked={position === 'fixed'}
+        onChange={(e) => setPosition(e.target.checked ? 'fixed' : 'static')}
+      />
+      <EuiSpacer />
+      <EuiHeader position={position} sections={sections} />
+    </>
+  );
+};
 
-// export default Header;
-export default {};
+export default Header;
       
